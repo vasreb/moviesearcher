52f28e474fd8d87c65fa7bf7a2c0e8dbd0de6cf6
vasreb <vasreborn@gmail.com>
2019-06-08 16:09:21 +0300

    init

---
+++ moviesearcher/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div``;
+
+function App() {
+  return <AppWrapper>s</AppWrapper>;
+}
+
+export default App;
