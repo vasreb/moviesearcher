a722bf483a046339e197969f7d2ed0ad148b162f
vasreb <vasreborn@gmail.com>
2019-06-25 17:14:43 +0300

    deploy server

---
+++ build/precache-manifest.690af7759c61098dc35ac614557bfcde.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ee928fb3c3d5fcc4b122293c54a208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "905f9362788d0eed66c4",
+    "url": "/static/css/3.764ccc25.chunk.css"
+  },
+  {
+    "revision": "67a2270565d035e3ca92",
+    "url": "/static/css/main.25e9666e.chunk.css"
+  },
+  {
+    "revision": "a8fecd56fdaa40110545",
+    "url": "/static/js/0.f1a982fc.chunk.js"
+  },
+  {
+    "revision": "905f9362788d0eed66c4",
+    "url": "/static/js/3.c5b78806.chunk.js"
+  },
+  {
+    "revision": "1ee35897334e83b40908",
+    "url": "/static/js/4.e16ebf64.chunk.js"
+  },
+  {
+    "revision": "61dd28eb3c444ae98888",
+    "url": "/static/js/5.3de0c16f.chunk.js"
+  },
+  {
+    "revision": "a66b477f3c02de12ffbd",
+    "url": "/static/js/6.154019a2.chunk.js"
+  },
+  {
+    "revision": "2c36892679b914fc6065",
+    "url": "/static/js/7.0713a79d.chunk.js"
+  },
+  {
+    "revision": "67a2270565d035e3ca92",
+    "url": "/static/js/main.3368fb68.chunk.js"
+  },
+  {
+    "revision": "3d31a4f1c761209f5588",
+    "url": "/static/js/runtime~main.a32e7a1b.js"
+  }
+]);
